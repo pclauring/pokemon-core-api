e71cb1124ca7bb228cfe552b7dac52c7b6a7baab
Pierce Lauring <pclauring@gmail.com>
2019-02-03 21:08:49 -0500

    added Newtonsoft Json to api

--- Controllers/PokemonController.cs
+++ Controllers/PokemonController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace pokemon_card.Controllers
 {
@@ -23,5 +24,27 @@ namespace pokemon_card.Controllers
 
         [HttpGet("species/{Id}")]
         public string GetPokemonSpeciesInfo(string Id) => client.GetStringAsync("https://pokeapi.co/api/v2/pokemon-species/" + Id).Result;
+
+        [HttpGet("full-pokemon/{Id}")]
+        public PokemonSpecies GetFullPokemon(string Id)
+        {
+
+            var response = client.GetStringAsync("https://pokeapi.co/api/v2/pokemon/" + Id).Result;
+            var pokemon = JsonConvert.DeserializeObject<PokemonSpecies>(response);
+            return pokemon;
+
+        }
+
+        public class PokemonSpecies
+        {
+            [JsonProperty("name")]
+            public string Name { get; set; }
+            [JsonProperty("id")]
+            public int PokemonId { get; set; }
+            [JsonProperty("height")]
+            public int Height { get; set; }
+            [JsonProperty("biome")]
+            public string Biome { get; set; }
+        }
     }
 }
